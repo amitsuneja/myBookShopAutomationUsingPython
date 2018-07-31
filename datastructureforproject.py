e69c699e9d6623129a64f384fa6398115819b4f0
Amit Suneja <amitsuneja.devops@gmail.com>
2018-07-31 05:36:09 +0530

    Create datastructureforproject.py

---
+++ datastructureforproject.py
@@ -0,0 +1,13 @@
+myshelf = {}
+myshelf["PY"] ={}
+myshelf["PY"]["WWW"] = {"Name": "Book1", "Class": "5th"}
+myshelf["PY"]["XXX"] = {"Name": "Book2", "Class": "6th"}
+
+myshelf["EE"] ={}
+myshelf["EE"]["YYY"] = {"Name": "Book3", "Class": "7th"}
+myshelf["EE"]["ZZZ"] = {"Name": "Book4", "Class": "8th"}
+
+print(myshelf)
+for shelfkey in myshelf.keys():
+    for subshelf in myshelf[shelfkey].keys():
+        print(myshelf[shelfkey][subshelf])
